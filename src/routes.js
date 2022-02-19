e042255ec141d80a0b959638fb0c52d635d5deef
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-02-20 00:47:21 +0100

    Add base structure & routes

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Chat from "./components/Chat";
+import Login from "./components/Login";
+import { LOGIN_ROUTE } from "./utils/consts";
+
+export const publicRoutes = [
+  {path: LOGIN_ROUTE, element: Login}
+]
+
+export const privateRoutes = [
+  {path: CHAT_ROUTE, element: Chat}
+]
